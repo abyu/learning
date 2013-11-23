9247f4d8ef44a7e8cad30729daf9826a798382c8
Kishore <abyu.kishore@thoughtworks.com>
2013-11-23 19:38:37 +0530

    Assignment 6 - Enhancement 2

--- ruby/assignment6/hw6assignment.rb
+++ ruby/assignment6/hw6assignment.rb
@@ -8,12 +8,43 @@ class MyPiece < Piece
 
   # your enhancements here
 
+  def self.my_next_piece (board)
+    Piece.new(All_My_Pieces.sample, board)
+  end
+
+  All_My_Pieces = All_Pieces +
+  [rotations([[0, 0], [1, 0], [0, 1], [1, 1], [2, 1]]),
+    [[[0, 0], [-1, 0], [1, 0], [2, 0], [3, 0]], [[0, 0], [0, -1], [0, 1], [0, 2], [0, 3]]],
+    rotations([[0,0], [0, 1], [1, 1], [1, 1]])]
+
 end
 
 class MyBoard < Board
   # your enhancements here
+ def initialize (game)
+  super(game)
+  @current_block = MyPiece.my_next_piece(self)
+ end
+
+   # gets the next piece
+  def next_piece
+    @current_block = MyPiece.my_next_piece(self)
+    @current_pos = nil
+  end
+
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    locations.each_with_index do |current, index| 
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    end
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
+
   def invert
-  	if !game_over? and @game.is_running?
+   if !game_over? and @game.is_running?
       @current_block.move(0, 0, 2)
     end
     draw
@@ -24,13 +55,12 @@ end
 class MyTetris < Tetris
   # your enhancements here
 
-	def initialize
-		super
-		set_my_board
-		my_key_bindings		
-	end
+ def initialize
+  super
+  my_key_bindings  
+ end
 
-	def set_my_board
+ def set_board
     @canvas = TetrisCanvas.new
     @board = MyBoard.new(self)
     @canvas.place(@board.block_size * @board.num_rows + 3,
